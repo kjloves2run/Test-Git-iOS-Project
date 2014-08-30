633e462304c23be3f3f5a6772d96b2ae2e9c81e0
Kevin Jordan <kevinjordan@Kevins-MacBook-Pro.local>
2014-08-30 13:13:04 -0500

    Added APPTest

---
+++ Git Test/APPTest.h
@@ -0,0 +1,13 @@
+//
+//  APPTest.h
+//  Git Test
+//
+//  Created by Kevin Jordan on 8/30/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APPTest : NSObject
+
+@end
---
+++ Git Test/APPTest.m
@@ -0,0 +1,13 @@
+//
+//  APPTest.m
+//  Git Test
+//
+//  Created by Kevin Jordan on 8/30/14.
+//
+//
+
+#import "APPTest.h"
+
+@implementation APPTest
+
+@end
